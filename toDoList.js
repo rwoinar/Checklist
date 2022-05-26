8cdc94fe1b5a7a346a146283cda04d7012adfaed
rwoinar <rwoinar@gmail.com>
2022-05-26 14:44:43 +0300

    functions for readability

--- toDoList.js
+++ toDoList.js
@@ -2,75 +2,73 @@ window.addEventListener('load', () => {
     
     let addTaskButton = document.getElementById('addTaskButton');
     let addButton = document.getElementById('add');
+    let currentDiv1 = document.getElementById("toDo");
     let currentDiv2 = document.getElementById('done');
+    let main = document.getElementById('toDo').parentNode;
     let cancelButton = document.getElementById('cancel');
-
-    addTaskButton.addEventListener('click', () => {
-        document.getElementsByClassName('addTask')[0].classList.add('visible-addTask'); 
-        document.getElementById('cover').style.display = 'block';
-    })
-
-
-    addButton.addEventListener('click', () => {
-        document.getElementById('cover').style.display = 'none';
-        const newDiv = document.createElement('div');
-        let newDivClass = document.createAttribute('class');
-        const newTask = document.createElement('input');
-        let taskType = document.createAttribute('type');
-        let newTaskLabel = document.createElement('label');
-        let newTaskContent = document.createTextNode(document.getElementById('newTaskInput').value);
-        
-        let editBox = document.createElement('input');
-        let editType = document.createAttribute('type');
-        let editClass = document.createAttribute('class');
-        editClass.value = 'editBox';
-        editType.value = 'text';
-        editBox.setAttributeNode(editClass);
-        editBox.setAttributeNode(editType);
-
-        const newCancel = document.createElement('button');
-        let cancelClass = document.createAttribute('class');
-        newCancel.innerHTML = 'x';
-
-        const currentDiv1 = document.getElementById("toDo");
-
-        const main = document.getElementById('toDo').parentNode;
-
-        newDivClass.value = 'task';
-        taskType.value = 'checkbox';
-        newDiv.setAttributeNode(newDivClass);
-        newTask.setAttributeNode(taskType);
-        newTaskLabel.appendChild(newTaskContent);
-
-        cancelClass.value = 'cancel';
-        newCancel.setAttributeNode(cancelClass);
     
+    function createDiv(){
+        newDiv = document.createElement('div');
+        newDivClass = document.createAttribute('class');
+        newDivClass.value = 'task';
+        newDiv.setAttributeNode(newDivClass);
+        return newDiv;
+    }
+
+    function createTask(){
+        newTask = document.createElement('input');
+        taskType = document.createAttribute('type');
+        taskType.value = 'checkbox';
+        newTask.setAttributeNode(taskType);
+        return newTask;
+    }
+    
+    function createTaskLabel(){
+        newTaskLabel = document.createElement('label');
+        newTaskContent = document.createTextNode(document.getElementById('newTaskInput').value);
+        newTaskLabel.appendChild(newTaskContent);
+        return newTaskLabel;
+    }
+
+    function createDel(){
+        newDel = document.createElement('button');
+        newDel.innerHTML = 'x';
+        delClass = document.createAttribute('class');
+        delClass.value = 'cancel';
+        newDel.setAttributeNode(delClass);
+        return newDel;
+    }
+
+    function appendAndInsert(){
+        newDiv.appendChild(newTask);
+        newDiv.appendChild(newTaskLabel);
+        newDiv.appendChild(newDel);
+        // newDiv.appendChild(editBox);
+        main.insertBefore(newDiv, currentDiv1);
+    }
+
+    function visibility(elem, visibility){
+        if(visibility){
+            elem.style.display = 'block';
+        }
+        else{
+            elem.style.display = 'none';
+        }
+    }
+
+    function mainCode(){
+        const newDiv = createDiv();
+        const newTask = createTask();
+        const newTaskLabel = createTaskLabel();
+        const newDel = createDel();
+
         if(document.getElementById('newTaskInput').value != ''){
-            newDiv.appendChild(newTask);
-            newDiv.appendChild(newTaskLabel);
-            newDiv.appendChild(newCancel);
-            newDiv.appendChild(editBox);
-            main.insertBefore(newDiv, currentDiv1);
+            appendAndInsert();
         }
 
-        document.getElementsByClassName('addTask')[0].classList.remove('visible-addTask');
-        document.getElementById('newTaskInput').value = ''
-
-        newCancel.addEventListener('click', () => {
+        newDel.addEventListener('click', () => {
             newDiv.remove();
         })
-
-        document.getElementsByClassName('addTask')[0].classList.remove('visible-addTask');
-        document.getElementById('newTaskInput').value = '';
-
-        // newDiv.addEventListener('dblclick', () => {
-        //     editBox.classList.add('editBox-visible');
-        //     editBox.value = newTaskLabel.innerHTML;
-        // })
-
-        // document.addEventListener('click', () => {
-            
-        // })
 
         newTask.addEventListener('change', () => {
             if(newTask.checked){
@@ -81,13 +79,48 @@ window.addEventListener('load', () => {
             }
         })
 
+        visibility(document.getElementById('cover'), false);
+        visibility(document.getElementsByClassName('addTask')[0], false);
+        document.getElementById('newTaskInput').value = '';
+    }
+
+    addTaskButton.addEventListener('click', () => {
+        visibility(document.getElementsByClassName('addTask')[0], true); 
+        visibility(document.getElementById('cover'), true);
+        document.addEventListener('keydown', (event) => {
+            if(event.code == 'Enter'){
+                event.preventDefault();
+                mainCode();
+            } 
+        })
+    })
+
+    addButton.addEventListener('click', () => {
+        mainCode();
+        // let editBox = document.createElement('input');
+        // let editType = document.createAttribute('type');
+        // let editClass = document.createAttribute('class');
+        
+        // editClass.value = 'editBox';
+        // editType.value = 'text';
+        // editBox.setAttributeNode(editClass);
+        // editBox.setAttributeNode(editType);
+
+        // newDiv.addEventListener('dblclick', () => {
+        //     editBox.classList.add('editBox-visible');
+        //     editBox.value = newTaskLabel.innerHTML;
+        // })
+
+        // document.addEventListener('click', () => {
+            
+        // })
+
     })  
 
     cancelButton.addEventListener('click', () => {
-        document.getElementsByClassName('addTask')[0].classList.remove('visible-addTask');
         document.getElementById('newTaskInput').value = '';
-        document.getElementById('cover').style.display = 'none';
+        visibility(document.getElementsByClassName('addTask')[0], false);
+        visibility(document.getElementById('cover'), false);
     })
-
 
 })
